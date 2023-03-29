98ff74e48bfd79fc0f857511fcd5b2ca26db547f
Tran Viet Huy <73055116+Andrew1013-development@users.noreply.github.com>
2023-03-29 18:20:31 +0700

    Delete bài nộp 29-3-2023 máy 06 directory

--- bài nộp 29-3-2023 máy 06/tong_so_chan_be_hon_n.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-s = 0
-i = 0
-while (i < n):
-    if n % 2 == 0:
-        s += i
-    i += 1
-print(s)
--- bài nộp 29-3-2023 máy 06/tong_tu_0_den_9.py
+++
@@ -1,6 +0,0 @@
-s = 0
-i = 0
-while (i <= 9):
-    s += i
-    i += 1
-print(s)
